3166cf5e022805d1d4c6cd2e56f3c3956114b5c9
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2022-12-02 00:22:42 +0530

    Added solution - LeetHub

---
+++ Check if it is possible to convert one string into another with given constraints - GFG/check-if-it-is-possible-to-convert-one-string-into-another-with-given-constraints.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            Solution ob = new Solution();
+            System.out.println(
+                ob.isItPossible(S[0], S[1], S[0].length(), S[1].length()));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int isItPossible(String S, String T, int M, int N) {
+        // code here
+        String s = S.replace("#","");
+        String s1 = T.replace("#","");
+        if(s1.equals(s)) return 1;
+        else return 0;
+    }
+};
