53233411a4c2ae140b57866063cb656d36289658
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2023-01-05 00:44:04 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find the longest string/find-the-longest-string.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class StringArray
+{
+    public static String[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        return s;
+    }
+
+    public static void print(String[] a)
+    {
+        for(String e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<String> a)
+    {
+        for(String e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            String[] arr = StringArray.input(br, n);
+            
+            Solution obj = new Solution();
+            String res = obj.longestString(n, arr);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static String longestString(int n, String[] arr) {
+        // code here
+        Arrays.sort(arr);
+        Set<String> set = new HashSet<>(Arrays.asList(arr));
+        String dada = "";
+        for (String s : arr) {
+            boolean found = true;
+            for (int i = 1; i < s.length(); i++) {
+                if (!set.contains(s.substring(0, i))) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found && s.length() > dada.length()) {
+                dada = s;
+            }
+        }
+        return dada;
+    }
+}
+        
