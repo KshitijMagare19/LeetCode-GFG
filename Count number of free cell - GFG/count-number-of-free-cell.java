e7d376fc7fe8c70835cc7dc4dc434c7843fe6c95
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2023-02-08 00:13:44 +0530

    Added solution - LeetHub

---
+++ Count number of free cell - GFG/count-number-of-free-cell.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int T = Integer.parseInt(in.readLine().trim());
+        while(T>0)
+        {
+            String s[]=in.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            int[][] a = new int[k][2];
+            for(int i=0; i<k; i++){
+                s=in.readLine().trim().split(" ");
+                a[i][0] = Integer.parseInt(s[0]);
+                a[i][1] = Integer.parseInt(s[1]);
+            }
+            Solution g = new Solution();
+            long[] res = g.countZero(n,k,a);
+            
+            for(int i=0; i<res.length; i++){
+                out.print(res[i]+" ");
+            }
+            out.println();
+            T--;
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    long[] countZero(int N, int K, int[][] arr){
+        // code here
+        long[] result = new long[K];
+        
+        boolean[] visitedRows = new boolean[N+1];
+        boolean[] visitedColumns = new boolean[N+1];
+        long numberOfNonVisitedRow = N;
+        long numberOfNonVisitedColumn = N;
+        
+        for(int i = 0; i < K; i++){
+            if(!visitedRows[arr[i][0]]){
+                visitedRows[arr[i][0]] = true;
+                numberOfNonVisitedRow--;
+            }
+            if(!visitedColumns[arr[i][1]]){
+                visitedColumns[arr[i][1]] = true;
+                numberOfNonVisitedColumn--;
+            }
+            result[i] = numberOfNonVisitedRow * numberOfNonVisitedColumn;
+        }
+        return result;
+    }
+}
