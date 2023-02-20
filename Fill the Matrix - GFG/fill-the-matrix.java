0dedd641ce896e94d1710a15c7cf360f72ae7d93
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2023-02-21 00:23:15 +0530

    Added solution - LeetHub

---
+++ Fill the Matrix - GFG/fill-the-matrix.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution
+{
+	public static int minIteration(int n, int m, int x, int y){
+		//code here
+		int ans=Math.max(Math.max(x-1+y-1,x-1+m-y),Math.max(n-x+y-1,n-x+m-y ));
+  return ans;
+		
+	}
+}
+
+//{ Driver Code Starts.
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+        	String input[]=read.readLine().trim().split(" ");
+            int N = Integer.parseInt(input[0]);
+            int M=Integer.parseInt(input[1]);
+           	input=read.readLine().trim().split(" ");
+            int x = Integer.parseInt(input[0]);
+            int y = Integer.parseInt(input[1]);
+            Solution ob = new Solution();
+            System.out.println(ob.minIteration(N, M, x, y));
+        }
+        
+    }
+}
+// } Driver Code Ends
