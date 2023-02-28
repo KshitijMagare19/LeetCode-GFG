d02571da198f6f925823fcf314a2d6cfc71ccfa2
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2023-03-01 00:43:57 +0530

    Added solution - LeetHub

---
+++ Update Queries - GFG/update-queries.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG{
+	public static void main(String [] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test = Integer.parseInt(br.readLine());
+		while(test-- > 0) {
+			String [] str = br.readLine().trim().split(" ");
+			int N = Integer.parseInt(str[0]);
+			int Q = Integer.parseInt(str[1]);
+			int [][] U = new int[Q][3];
+			int i = 0;
+			while(i < Q) {
+				str = br.readLine().trim().split(" ");
+				U[i][0] = Integer.parseInt(str[0]);
+				U[i][1] = Integer.parseInt(str[1]);
+				U[i][2] = Integer.parseInt(str[2]);
+				i++;
+			}
+			Solution obj = new Solution();
+			int [] arr = obj.updateQuery(N, Q, U);
+			for(int j: arr) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+	int [] updateQuery(int N, int Q, int [][]U){
+        //Write your code here
+        int[] a=new int[N];
+
+        
+
+        for(int i=0;i<Q;i++){
+
+            int l=U[i][0]-1;
+
+            int r=U[i][1];
+
+            int x=U[i][2];
+
+            
+
+            for(int j=l;j<r;j++){
+
+                a[j] |=x;
+
+            }
+
+        }
+
+        return a;
+    }
+}
