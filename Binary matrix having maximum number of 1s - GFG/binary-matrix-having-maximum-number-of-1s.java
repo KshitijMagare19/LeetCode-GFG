12f9d25f908f276964b96102a4b2e1a9f72c03f7
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2023-03-12 01:21:18 +0530

    Added solution - LeetHub

---
+++ Binary matrix having maximum number of 1s - GFG/binary-matrix-having-maximum-number-of-1s.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+class FastReader{ 
+    BufferedReader br; 
+    StringTokenizer st; 
+
+    public FastReader(){ 
+        br = new BufferedReader(new InputStreamReader(System.in)); 
+    } 
+
+    String next(){ 
+        while (st == null || !st.hasMoreElements()){ 
+            try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    String nextLine(){ 
+        String str = ""; 
+        try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+        return str; 
+    } 
+    
+    Integer nextInt(){
+        return Integer.parseInt(next());
+    }
+} 
+
+class GfG {
+    public static void main(String[] args) {
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            int B = sc.nextInt();
+            int arr[][] = new int[B][B];
+            for(int i = 0; i < B; i++) {
+                for(int j = 0; j < B; j++) {
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+            Solution obj = new Solution();
+            int ans[] = obj.findMaxRow(arr, B);
+            for(int i = 0; i < 2; i++) {
+                out.print(ans[i] + " ");
+            }
+            out.println();
+        }
+        out.flush();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public int[] findMaxRow(int mat[][], int N) {
+        // code here
+        int row = 0 ,cnt = 0, t1 = 0, t2 = 0;
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                if(mat[i][j] == 1) t1++;
+            }
+            if(t1 > cnt) row = i;
+            cnt = Math.max(cnt,t1);
+            t1 = 0;
+        }
+        int ans[] = new int[2];
+        ans[0] = row;
+        ans[1] = cnt;
+        return ans;
+    }
+};
