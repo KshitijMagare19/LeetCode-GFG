0e9e50a1e2b9713b2a570557ab964506b74bce42
KshitijMagare19 <kshitij.magare19@vit.edu>
2023-04-18 00:32:47 +0530

    Added solution - CodeHub

---
+++ gfg/pass-by-reference-and-value/solution.java
@@ -0,0 +1,11 @@
+...// } Driver Code Ends
+// User function Template for Java
+class Solution {
+    static int[] passedBy(int a, int b) {
+        // code here
+        int[] ans = new int[2];
+        ans[0] = a+1;
+        ans[1] = b+2;
+        return ans;
+    }
+}
