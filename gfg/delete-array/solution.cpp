546ba4d8158ec167aef58c2026476ea88299bff2
KshitijMagare19 <kshitij.magare19@vit.edu>
2023-04-18 00:30:53 +0530

    Added solution - CodeHub

---
+++ gfg/delete-array/solution.cpp
@@ -0,0 +1,7 @@
+...// } Driver Code Ends
+//User function Template for C++
+// Code to delete the array
+// Task is to comment the line which deletes 
+// the array such that required output is printed
+//delete[] arr; //comment this line so memory doesn't get deallocated
+...// } Driver Code Ends
