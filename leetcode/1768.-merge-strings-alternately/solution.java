645ce876a5eecf352c30500cdbb9e246c7e06c96
KshitijMagare19 <kshitij.magare19@vit.edu>
2023-04-18 23:36:03 +0530

    Added solution - CodeSyncer

--- leetcode/1768.-merge-strings-alternately/solution.java
+++ leetcode/1768.-merge-strings-alternately/solution.java
@@ -1,14 +1,12 @@
 class Solution {
-    public String mergeAlternately(String word1, String word2) {
-        String ans = "";
-        int i = 0, j = 0, k = 0;
-        int len = Math.min(word1.length(),word2.length());
-        for(;i<len;i++){
-            ans+=word1.charAt(k++);
-            ans+=word2.charAt(j++);
+    public String mergeAlternately(String w1, String w2) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < w1.length() || i < w2.length(); ++i) {
+            if (i < w1.length())
+                res.append(w1.charAt(i));
+            if (i < w2.length())
+                res.append(w2.charAt(i));
         }
-        if(word1.length() > word2.length()) ans+= word1.substring(k);
-        else ans+= word2.substring(j);
-        return ans;
+        return res.toString();
     }
 }
