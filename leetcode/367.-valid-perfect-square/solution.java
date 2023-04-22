9a624e40be35f7cbfefb192a9af6f33012df537f
KshitijMagare19 <kshitij.magare19@vit.edu>
2023-04-22 23:45:45 +0530

    Added solution - CodeSyncer

--- leetcode/367.-valid-perfect-square/solution.java
+++ leetcode/367.-valid-perfect-square/solution.java
@@ -1,15 +1,6 @@
 class Solution {
-    public boolean isPerfectSquare(int num) {
-        int left = 1, right = 46340; // sqrt(2^31 - 1) < 46340 
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (mid * mid == num)
-                return true;
-            if (mid * mid < num)
-                left = mid + 1;
-            else
-                right = mid - 1;
-        }
-        return false;
+    public int[] runningSum(int[] nums) {
+        for(int i=1; i<nums.length; i++) nums[i] += nums[i-1];
+	    return nums;
     }
 }
