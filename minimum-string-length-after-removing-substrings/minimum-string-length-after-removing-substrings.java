3d99a9e5332da06c786b56e59ba26c6a2775eb19
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2023-05-22 23:38:50 +0530

    Time: 60 ms (14.29%) | Memory: 44.1 MB (14.29%) - LeetSync

---
+++ minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minLength(String s) {
+        //int ans = 0;
+        while(s.contains("AB") || s.contains("CD")){
+            if(s.contains("AB")){
+                //ans++;
+                s = s.replace("AB","");
+                System.out.println(s);
+            }
+            else{
+                //ans++;
+                s = s.replace("CD","");
+                System.out.println(s);
+            }
+        }
+        System.out.println(s);
+        return s.length();
+    }
+}
