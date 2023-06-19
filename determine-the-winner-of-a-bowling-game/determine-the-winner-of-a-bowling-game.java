26523b76c9e24bcb9e87f1d008453c7908f72fa7
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2023-06-20 01:09:37 +0530

    Time: 12 ms (7.76%) | Memory: 44.9 MB (5.66%) - LeetSync

--- determine-the-winner-of-a-bowling-game/determine-the-winner-of-a-bowling-game.java
+++ determine-the-winner-of-a-bowling-game/determine-the-winner-of-a-bowling-game.java
@@ -2,52 +2,51 @@ class Solution {
     public int isWinner(int[] p1, int[] p2) {
         int s1 = 0, s2 = 0, cnt = 0;
         for(int i : p1){
-                if(i == 10 && cnt < 1){
-                    cnt = 2;
-                    s1+= i;
-                }
-                else if(i == 10 && cnt < 1){
-                    cnt = 2;
-                    s1+= i;
-                }
-                else if(i == 10 && cnt > 0){
-                    cnt = 2;
-                    s1+= i*2;
-                }
-                else if(cnt > 0){
-                    s1+=(2*i);
-                    cnt--;
-                }
-                else{
-                    s1+=i;
-                }
-                
-            } 
-            cnt = 0;
-            for(int i : p2){
-                if(i == 10 && cnt < 1){
-                    cnt = 2;
-                    s2+= i;
-                }
-                else if(i == 10 && cnt < 1){
-                    cnt = 2;
-                    s2+= i;
-                }
-                else if(i == 10 && cnt > 0){
-                    cnt = 2;
-                    s2+= i*2;
-                }
-                else if(cnt > 0){
-                    s2+=(2*i);
-                    cnt--;
-                }
-                else{
-                    s2+=i;
-                }
+            if(i == 10 && cnt < 1){
+                cnt = 2;
+                s1+= i;
             }
-            System.out.println(s1 + " "+ s2);
-            if(s1>s2) return 1;
-            else if(s1 == s2) return 0;
-            else return 2;
+            else if(i == 10 && cnt < 1){
+                cnt = 2;
+                s1+= i;
+            }
+            else if(i == 10 && cnt > 0){
+                cnt = 2;
+                s1+= i*2;
+            }
+            else if(cnt > 0){
+                s1+=(2*i);
+                cnt--;
+            }
+            else{
+                s1+=i;
+            }  
+        } 
+        cnt = 0;
+        for(int i : p2){
+            if(i == 10 && cnt < 1){
+                cnt = 2;
+                s2+= i;
+            }
+            else if(i == 10 && cnt < 1){
+                cnt = 2;
+                s2+= i;
+            }
+            else if(i == 10 && cnt > 0){
+                cnt = 2;
+                s2+= i*2;
+            }
+            else if(cnt > 0){
+                s2+=(2*i);
+                cnt--;
+            }
+            else{
+                s2+=i;
+            }
         }
+        System.out.println(s1 + " "+ s2);
+        if(s1>s2) return 1;
+        else if(s1 == s2) return 0;
+        else return 2;
     }
+}
