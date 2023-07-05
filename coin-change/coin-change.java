8493b3bd9427e615b40f87cf6c02dae297ae225d
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2023-07-06 00:44:11 +0530

    Time: 33 ms (28.40%) | Memory: 44.4 MB (9.59%) - LeetSync

--- coin-change/coin-change.java
+++ coin-change/coin-change.java
@@ -1,17 +1,35 @@
 class Solution {
+
+    
     public int coinChange(int[] coins, int amount) {
-        int dp[] = new int[amount + 1];
-        Arrays.fill(dp,amount+1);
-        dp[0] = 0;
-        for(int i = 1; i <= amount; i++){
-            for(int j = 0 ; j < coins.length; j++){
-                if(i >= coins[j]){
-                    dp[i] = Math.min(dp[i], dp[i-coins[j]]+ 1);
+        int[][] dp = new int[coins.length + 1][amount+1];
+        for(int i = 0; i < coins.length + 1; i++){
+            for(int j = 0; j < amount + 1 ; j++){
+                if(j == 0) dp[i][j] = 0;
+                if(i == 0) dp[i][j] = Integer.MAX_VALUE - 1;
+            }
+        }
+        for(int j = 1; j < amount +1; j++){
+            if(j % coins[0] == 0){
+                dp[1][j] = j/coins[0];
+            }
+            else{
+                dp[1][j] = Integer.MAX_VALUE - 1;
+            }
+        }
+
+        //System.out.println(Arrays.toString(dp));
+        for(int i = 2; i < coins.length + 1; i++){
+            for(int j = 1; j < amount + 1 ; j++){
+                if(coins[i-1] <= j){
+                     dp[i][j] = Math.min(dp[i-1][j] , 1 + dp[i][j-coins[i-1]]);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
-        //System.out.println(Arrays.toString(dp));
-        if(amount < dp[amount]) return -1;
-        else return dp[amount];
+        if(dp[coins.length][amount] == Integer.MAX_VALUE - 1) return -1;
+        return dp[coins.length][amount];
     }
 }
