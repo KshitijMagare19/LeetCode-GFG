2de2de442c5a97fd5a4409692f4e7813e2d23e24
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2023-07-21 01:57:12 +0530

    Added solution - LeetHub

---
+++ Arrays (Sum of array) - GFG/arrays-sum-of-array.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.getSum(a, n));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    public long getSum(long a[], long n)
+    {
+        // Your code goes here
+        long ans = 0;
+        for(long i : a) ans+= i;
+        return ans;
+    }
+}
