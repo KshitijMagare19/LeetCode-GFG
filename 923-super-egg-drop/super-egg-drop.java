e1a755c502560984d81bf2ae2c65c7a7f696f7cf
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2023-07-22 00:05:19 +0530

    Time: 1234 ms (5.11%) | Memory: 162.7 MB (5.11%) - LeetSync

---
+++ 923-super-egg-drop/super-egg-drop.java
@@ -0,0 +1,46 @@
+class Solution {
+    int[][] dp = new int[101][100001];
+    public int solve(int e, int f){
+        if(f == 0 || f == 1) 
+            return f;
+        if(e == 1)
+            return f;
+        if(dp[e][f] != -1) 
+            return dp[e][f];
+
+        int ans = Integer.MAX_VALUE;
+        int l=1, r=f;
+        while(l<=r){
+            int k = (l+r)/2;
+            int low = 0, high = 0;
+            if(dp[e-1][k-1]!= -1)
+                low = dp[e-1][k-1];
+            else{
+                low = solve(e-1,k-1);
+                dp[e-1][k-1] = low;
+            }
+            if(dp[e][f-k]!= -1)
+                high = dp[e][f-k];
+            else{
+                high = solve(e,f-k);
+                dp[e][f-k] = high;
+            }
+            
+            int temp = 1 + Math.max(low,high);
+            ans = Math.min(ans,temp);
+            if(low<high){
+                l = k+1;
+            }else{
+                r = k-1;
+            }
+        }
+        dp[e][f] = ans;
+        return ans;
+    }
+    public int superEggDrop(int e, int f) {
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
+        }
+        return solve(e,f);
+    }
+}
