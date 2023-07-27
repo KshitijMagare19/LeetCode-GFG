3bf02d570461f9c01954ff31cb3f2a214c04b7eb
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2023-07-27 20:39:49 +0530

    Added solution - LeetHub

---
+++ Twice Counter - GFG/twice-counter.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])throws IOException
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n = sc.nextInt();
+                    String list[] = new String[n];
+                    for(int i = 0;i<n;i++)
+                        list[i] = sc.next();
+                    Solution ob = new Solution();
+                    System.out.println(ob.countWords(list, n));
+                }
+        }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int countWords(String list[], int n)
+    {
+        // code here
+        int ans = 0;
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String s : list){
+            if(map.containsKey(s)){
+                map.put(s,map.get(s)+1);
+            }
+            else{
+                map.put(s,1);
+            }
+        }
+        for (Map.Entry<String,Integer> entry : map.entrySet()) 
+            if(entry.getValue() == 2) ans++;
+    
+        return ans;
+    }
+    
+}
