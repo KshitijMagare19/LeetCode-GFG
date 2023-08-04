6fe9f06c29058d11a197901ac32fec79901f0368
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2023-08-05 01:15:14 +0530

    Time: 703 ms (5.16%) | Memory: 163.1 MB (5.56%) - LeetSync

--- 923-super-egg-drop/super-egg-drop.java
+++ 923-super-egg-drop/super-egg-drop.java
@@ -1,36 +1,38 @@
 class Solution {
     int[][] dp = new int[101][100001];
     public int solve(int e, int f){
-        if(f == 0 || f == 1 || e == 1) 
+        if(e == 1 || f == 0 || f == 1){
             return f;
-        if(dp[e][f] != -1) 
+        }
+        if(dp[e][f] != -1){
             return dp[e][f];
-
+        }
         int ans = Integer.MAX_VALUE;
-        int l=1, r=f;
+        int l = 1, r = f;
         while(l<=r){
             int k = (l+r)/2;
             int low = 0, high = 0;
-            if(dp[e-1][k-1]!= -1)
+            if(dp[e-1][k-1] != -1){
                 low = dp[e-1][k-1];
+            }
             else{
                 low = solve(e-1,k-1);
                 dp[e-1][k-1] = low;
             }
-            if(dp[e][f-k]!= -1)
+            if(dp[e][f-k] != -1){
                 high = dp[e][f-k];
+            }
             else{
                 high = solve(e,f-k);
                 dp[e][f-k] = high;
             }
-            
-            int temp = 1 + Math.max(low,high);
-            ans = Math.min(ans,temp);
+            int temp = 1 + Math.max(high,low);
+            ans = Math.min(temp,ans);
             if(low<high){
                 l = k+1;
             }else{
                 r = k-1;
-            }
+            }         
         }
         dp[e][f] = ans;
         return ans;
