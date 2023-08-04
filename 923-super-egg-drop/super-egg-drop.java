610c5ad8dd04add9f9d1fe015ffffc5499c42c44
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2023-08-05 01:27:33 +0530

    Time: 872 ms (5.16%) | Memory: 162.3 MB (5.56%) - LeetSync

--- 923-super-egg-drop/super-egg-drop.java
+++ 923-super-egg-drop/super-egg-drop.java
@@ -9,30 +9,30 @@ class Solution {
         }
         int ans = Integer.MAX_VALUE;
         int l = 1, r = f;
-        while(l<=r){
-            int k = (l+r)/2;
+        while(l <= r){
             int low = 0, high = 0;
-            if(dp[e-1][k-1] != -1){
+            int k = (l+r)/2;
+            if(dp[e-1][k-1]!= -1)
                 low = dp[e-1][k-1];
-            }
             else{
                 low = solve(e-1,k-1);
                 dp[e-1][k-1] = low;
             }
-            if(dp[e][f-k] != -1){
+            if(dp[e][f-k]!= -1)
                 high = dp[e][f-k];
-            }
             else{
                 high = solve(e,f-k);
                 dp[e][f-k] = high;
             }
-            int temp = 1 + Math.max(high,low);
+            
+            int temp = 1 + Math.max(low,high);
             ans = Math.min(temp,ans);
-            if(low<high){
+            if(low < high){
                 l = k+1;
-            }else{
+            }
+            else{
                 r = k-1;
-            }         
+            }
         }
         dp[e][f] = ans;
         return ans;
