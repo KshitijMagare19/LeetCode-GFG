234dbc98e13e3de3c356276fec7b9f259800f88e
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2023-08-05 23:36:39 +0530

    Time: 83 ms (18.85%) | Memory: 55.1 MB (5.95%) - LeetSync

--- 923-super-egg-drop/super-egg-drop.java
+++ 923-super-egg-drop/super-egg-drop.java
@@ -1,32 +1,33 @@
 class Solution {
-    int[][] dp = new int[101][100001];
+    int[][] dp = new int[101][10001];
     public int solve(int e, int f){
-        if(e == 1 || f == 0 || f == 1){
+        if(f == 0 || f == 1 || e == 1){
             return f;
         }
         if(dp[e][f] != -1){
             return dp[e][f];
         }
-        int ans = Integer.MAX_VALUE;
         int l = 1, r = f;
-        while(l <= r){
+        int ans = Integer.MAX_VALUE;
+        while(l<=r){
             int low = 0, high = 0;
             int k = (l+r)/2;
-            if(dp[e-1][k-1]!= -1)
+            if(dp[e-1][k-1] != -1){
                 low = dp[e-1][k-1];
+            }
             else{
                 low = solve(e-1,k-1);
                 dp[e-1][k-1] = low;
             }
-            if(dp[e][f-k]!= -1)
+            if(dp[e][f-k] != -1){
                 high = dp[e][f-k];
+            }
             else{
                 high = solve(e,f-k);
                 dp[e][f-k] = high;
             }
-            
-            int temp = 1 + Math.max(low,high);
-            ans = Math.min(temp,ans);
+            int temp = 1 + Math.max(high,low);
+            ans = Math.min(ans,temp);
             if(low < high){
                 l = k+1;
             }
@@ -37,6 +38,7 @@ class Solution {
         dp[e][f] = ans;
         return ans;
     }
+
     public int superEggDrop(int e, int f) {
         for(int[] row : dp){
             Arrays.fill(row,-1);
