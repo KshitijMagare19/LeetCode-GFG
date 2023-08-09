1ecd56e55e4cca242f848a4d5528541020bc6dde
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2023-08-10 02:10:00 +0530

    Time: 0 ms (100.00%) | Memory: 39.6 MB (27.68%) - LeetSync

--- 2955-account-balance-after-rounded-purchase/account-balance-after-rounded-purchase.java
+++ 2955-account-balance-after-rounded-purchase/account-balance-after-rounded-purchase.java
@@ -1,11 +1,9 @@
 class Solution {
     public int accountBalanceAfterPurchase(int purchaseAmount) {
-        if(purchaseAmount % 10 < 5){
-            purchaseAmount-=(purchaseAmount%10);
-        }
-        else{
-            purchaseAmount+=(10-(purchaseAmount%10));
-        }
+        purchaseAmount = purchaseAmount % 10 < 5 ?
+            purchaseAmount - (purchaseAmount%10):
+            purchaseAmount + (10-(purchaseAmount%10));
+        
         return 100-purchaseAmount;
     }
 }
