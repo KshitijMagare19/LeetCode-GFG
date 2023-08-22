fee76112a75ab3a7069577502d273f963aedc94c
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2023-08-23 02:27:43 +0530

    Time: 0 ms (100.00%) | Memory: 39.6 MB (21.66%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int count = numBottles;
+        
+        while(numBottles>=numExchange){
+            int x = numBottles/numExchange;
+            count+=x;
+            numBottles = x + (numBottles - numExchange*x);
+        }
+        
+        return count;
+        
+    }
+}
