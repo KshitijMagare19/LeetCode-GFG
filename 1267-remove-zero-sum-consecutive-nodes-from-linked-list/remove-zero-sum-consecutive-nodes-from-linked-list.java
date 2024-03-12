b71898ca2fe5cce5f43f207ad89f195d5d55c57e
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2024-03-13 00:34:01 +0530

    Time: 3 ms (54.35%) | Memory: 44.1 MB (40.71%) - LeetSync

---
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeZeroSumSublists(ListNode head) {
+        int prefixSum = 0;
+        HashMap<Integer, ListNode> map = new HashMap<>();
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        map.put(0, dummy);
+        while (head != null) {
+            prefixSum += head.val;
+            if (map.containsKey(prefixSum)) {
+                ListNode start = map.get(prefixSum);
+                ListNode temp = start;
+                int sum = prefixSum;
+                while (temp != head) {
+                    temp = temp.next;
+                    sum += temp.val;
+                    if (temp != head)
+                        map.remove(sum);
+                }
+                start.next = head.next;
+            } else {
+                map.put(prefixSum, head);
+            }
+            head = head.next;
+        }
+        return dummy.next;
+    }
+}
