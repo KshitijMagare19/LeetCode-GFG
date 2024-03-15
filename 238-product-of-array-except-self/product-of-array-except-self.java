601f622d0e6d9cf70844dca7c87a93bceca11520
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2024-03-16 01:40:35 +0530

    Time: 4 ms (6.81%) | Memory: 54.6 MB (27.11%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -4,13 +4,14 @@ class Solution {
         int[] rightProduct = new int[nums.length];
         int len = nums.length;
         Arrays.fill(leftProduct,1);
-         Arrays.fill(rightProduct,1);
-        for(int i = 1; i < len ; i++){
+        Arrays.fill(rightProduct,1);
+        for(int i = 1,j=len-2; i < len && j>=0 ; i++,j--){
             leftProduct[i]=leftProduct[i-1]*nums[i-1];
-        }
-        for(int j=len-2;  j>=0 ; j--){
             rightProduct[j]=rightProduct[j+1]*nums[j+1];
         }
+        // for(int ;  j>=0 ; j--){
+            
+        // }
         //System.out.println(Arrays.toString(rightProduct));
         int[] ans = new int[len];
         for(int i = 0; i < len; i++){
