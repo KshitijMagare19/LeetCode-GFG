6cd0b40b1b059cfbcf62bb6b3556dcaae89fcfb6
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2024-04-01 23:20:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (18.81%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,7 +1,19 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] arr = s.split(" ");
-        int n = arr.length;
-        return arr[n-1].length();
+        // String[] arr = s.split(" ");
+        // int n = arr.length;
+        // return arr[n-1].length();
+        int length = 0;
+		
+		// We are looking for the last word so let's go backward
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') { // a letter is found so count
+                length++;
+            } else {  // it's a white space instead
+				//  Did we already started to count a word ? Yes so we found the last word
+                if (length > 0) return length;
+            }
+        }
+        return length;
     }
 }
