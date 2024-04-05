83a5d1eba7fa6dbb51536146e8d121dd571d782e
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2024-04-05 23:17:30 +0530

    Time: 1 ms (100.00%) | Memory: 42.3 MB (39.72%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.java
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -1,17 +1,31 @@
 class Solution {
     public String makeGood(String s) {
-        Stack<Character> stack = new Stack();
-        for(int i=0;i<s.length();i++){
-            if(!stack.isEmpty() && Math.abs(stack.peek()-s.charAt(i)) == 32)
-                stack.pop();
+       if (s.length() == 0 || s.length() == 1)
+            return s;
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(s);
+
+        int i = 0;
+
+        while (i < sb.length() - 1)
+        {
+            char leftChar = sb.charAt(i);
+            char rightChar = sb.charAt(i + 1);
+
+            if (Math.abs(leftChar - rightChar) == 32)
+            {
+                sb.delete(i, i + 2);
+                if (i > 0)
+                    i--;
+            }
             else
-                stack.push(s.charAt(i));
+            {
+                i++;
+            } 
         }
-        char res[] = new char[stack.size()];
-        int index = stack.size()-1;
-        while(!stack.isEmpty()){
-            res[index--] = stack.pop();
-        }
-        return new String(res);
+
+        return sb.toString();
     }
 }
