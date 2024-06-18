1df448bd3974791b61f21858089cd3e2a74627be
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2024-06-19 00:53:57 +0530

    Time: 67 ms (23.09%), Space: 46.1 MB (13.27%) - LeetHub

---
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.java
@@ -0,0 +1,18 @@
+class Solution {
+        public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        map.put(0, 0);
+        for (int i = 0; i < difficulty.length; i++) {
+            map.put(difficulty[i], Math.max(profit[i], map.getOrDefault(difficulty[i], 0)));
+        }
+        int best = 0, res = 0;
+        for (Integer key : map.keySet()) {
+            best = Math.max(map.get(key), best);
+            map.put(key, best);
+        }
+        for (int i = 0; i < worker.length; i++) {
+            res += map.floorEntry(worker[i]).getValue();
+        }
+        return res;
+    }
+}
