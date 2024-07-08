67b7daa20e208326c0dc36518edea94564fbe451
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2024-07-09 00:29:42 +0530

    Time: 0 ms (100%), Space: 40.6 MB (42.7%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -3,9 +3,10 @@ class Solution {
     return helper(n,k)+1;
 }
 public int helper(int n,int k){
-    if(n==1)
-        return 0;
-    else
-        return (helper(n-1,k)+k)%n;
+    return (n == 1) ? 0 : (helper(n-1,k)+k)%n;
+    // if(n==1)
+    //     return 0;
+    // else
+    //     return (helper(n-1,k)+k)%n ;
 }
 }
