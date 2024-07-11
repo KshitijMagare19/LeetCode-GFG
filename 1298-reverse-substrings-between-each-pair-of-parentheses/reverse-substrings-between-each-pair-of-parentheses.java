68ee0fe2653a11108bf58f67235e1925ddceec9b
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2024-07-12 01:11:31 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (49.40%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
@@ -1,52 +1,28 @@
 class Solution {
-    public String rev(String s){
-        StringBuffer  str =new StringBuffer(s); 
-        str = str.reverse();
-         return str.toString();
+    int i = 0;
+    public String reverseParentheses(String s) {
+        char[] ar = s.toCharArray();
+        return helper(ar);
     }
 
-    public String reverseParentheses(String s) {
-        StringBuffer  str =new StringBuffer(s); 
-        String ans = "";
-        int start = 0, end = 0; 
-        int cnt = 0;
-        for(int i = 0; i < s.length(); i++ ) if(s.charAt(i) == '(') cnt++;
-        if (cnt == 0) return s;
-        if(cnt < 2) {
-            for(int i = 0; i < s.length(); i++ ){
-                char c = s.charAt(i);
-                if(c == '(') start = i;
-                else if(c == ')') {
-                    end = i;
-                    break;
-                }
+    public String helper(char[] s){
+        StringBuilder sb = new StringBuilder();
+
+        while(i < s.length){
+            if(s[i] == ')'){
+                i++;
+                return sb.reverse().toString();
+            }else if(s[i] == '('){
+                i++;
+                String st  = helper(s);
+                //System.out.println(st);
+                sb.append(st);
+            }else{
+                sb.append(s[i]);
+                i++;
             }
-            int st = 0, ed = s.length(); 
-            if(start != 0) st = start;
-            if(end != s.length()) ed = end;
-            ans  = s.substring(0,st) + rev(s.substring(st+1,end)) + s.substring(ed+1,s.length() );
-            s = ans;
-            return s;
         }
-        else{
-        while(cnt-- > 0){
-            for(int i = 0; i < s.length(); i++ ){
-                char c = s.charAt(i);
-                if(c == '(') start = i;
-                else if(c == ')') {
-                    end = i;
-                    break;
-                }
-            }
-            
-            ans  = s.substring(0,start) + rev(s.substring(start+1,end)) + s.substring(end + 1,s.length() );
-            // System.out.println( s.substring(0,start)+ " - "+
-            // s.substring(start+1,end) + " - "+ s.substring(end,s.length()) + " s: "+start+" e: "+end +"\n"+ ans);
-            s = ans;
-            // String str = str.substring(0,start) + 
-            //str.substring(start,end).reverse() + str.substring(end);
-        }
-        }
-        return s;
+        return sb.toString();
+
     }
 }
