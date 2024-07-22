a3e5f99f65489ce7208a4b685a9005515c9097ba
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2024-07-23 00:56:31 +0530

    Time: 18 ms (34.18%), Space: 44.9 MB (91.7%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.java
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -1,16 +1,29 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        HashMap<Integer,String> map = new HashMap<>();
+        // HashMap<Integer,String> map = new HashMap<>();
+        // for(int i = 0; i<names.length; i++){
+        //     map.put(heights[i],names[i]);
+        // }
+        // String[] ans = new String[names.length];
+        // Arrays.sort(heights);
+        // int j = 0;
+        // for(int i = heights.length -1 ; i>=0; i--){
+        //     ans[j] = map.get(heights[i]);
+        //     j++;
+        // }
+        // return ans;
+        String[] ans = new String[names.length];
+        TreeMap<Integer,String>  map = new TreeMap<>();
         for(int i = 0; i<names.length; i++){
             map.put(heights[i],names[i]);
         }
-        String[] ans = new String[names.length];
-        Arrays.sort(heights);
-        int j = 0;
-        for(int i = heights.length -1 ; i>=0; i--){
-            ans[j] = map.get(heights[i]);
-            j++;
-        }
+        int j = names.length - 1 ;
+        System.out.println(map);
+        for (Map.Entry<Integer,String> entry : map.entrySet()){
+
+            ans[j--] = entry.getValue(); 
+        } 
+
         return ans;
         
     }
