3ba13ec300e7bcd629bffc0d9da2a98096a8857c
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2024-08-20 01:38:21 +0530

    Time: 83 ms (5.05%) | Memory: 55.6 MB (8.45%) - LeetSync

---
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -0,0 +1,36 @@
+class Solution {
+    int[][] dp = new int[1001][1001]; 
+    
+    int solve(int currA, int copy, int n){
+        if(currA == n){
+            return 0;
+        }
+        
+        if(currA > n){
+            return 1001;
+        }
+        if(dp[currA][copy] != -1){
+            return dp[currA][copy] ;
+        }
+
+        int CACC = 1 + 1 + solve(2*currA, currA, n);
+
+        int CV = 1 + solve(currA + copy, copy, n);
+
+        return dp[currA][copy] = Math.min(CACC, CV);
+
+    }
+    public int minSteps(int n) {
+        //bottom up DP
+        if(n == 1)  return 0;
+
+        for(int[] i :dp){
+            Arrays.fill(i,-1);
+        }
+
+        return 1 + solve(1,1,n);
+
+
+    }
+}
+//27
