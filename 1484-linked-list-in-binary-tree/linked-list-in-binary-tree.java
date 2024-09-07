1b10d1ca59ba9be766e6e98cd4e6644bff07de51
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2024-09-07 23:49:36 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (91.36%) - LeetSync

---
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        return dfs(head, head, root);  // Start DFS search
+    }
+
+    // DFS helper function
+    boolean dfs(ListNode head, ListNode cur, TreeNode root) {
+        // If we reach the end of the linked list, return true (successful match)
+        if (cur == null) return true;
+        // If we reach the end of a path in the tree without fully matching the list, return false
+        if (root == null) return false;
+        
+        // Match the current tree node with the current linked list node
+        if (cur.val == root.val) cur = cur.next;
+        // If no match, but the tree node matches the head of the linked list, start a new match
+        else if (head.val == root.val) head = head.next;
+        // Otherwise, reset `cur` to `head` to attempt matching the linked list from scratch
+        else cur = head;
+
+        // Continue DFS down both left and right children
+        return dfs(head, cur, root.left) || dfs(head, cur, root.right);
+    }
+}
