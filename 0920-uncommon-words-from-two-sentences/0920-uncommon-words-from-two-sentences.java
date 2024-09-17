c683034519d75dcaaea3b862f927a8a4819c4f58
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2024-09-18 02:21:55 +0530

    Time: 3 ms (38.73%), Space: 42.1 MB (34.64%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
@@ -9,25 +9,19 @@ class Solution {
             wordFreq1.put(word, wordFreq1.getOrDefault(word, 0)+1);
         }
         for(var word : sen2Words){
-            wordFreq2.put(word, wordFreq2.getOrDefault(word, 0)+1);
+            wordFreq1.put(word, wordFreq1.getOrDefault(word, 0)+1);
         }
         List<String> uncommons = new ArrayList<>();
         
         for(var entry : wordFreq1.entrySet()){
             String word = entry.getKey();
             int wordFreq = entry.getValue();
-            if(wordFreq == 1 && !wordFreq2.containsKey(word)){
+            if(wordFreq == 1 ){
                  uncommons.add(word);
             }
         }
        
-        for(var entry : wordFreq2.entrySet()){
-            String word = entry.getKey();
-            int wordFreq = entry.getValue();
-            if(wordFreq == 1 && !wordFreq1.containsKey(word)){
-                 uncommons.add(word);
-            }
-        }
+        
         return uncommons.stream().toArray(String[]::new);
     }
 }
