07e5e40fe1f0973c7033f79a4138b2c8132718b9
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2024-10-21 01:21:15 +0530

    Time: 91 ms (12.32%), Space: 26.7 MB (5.28%) - LeetHub

---
+++ 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.py
@@ -0,0 +1,4 @@
+class Solution:
+    def parseBoolExpr(self, S, t=True, f=False):
+        return eval(S.replace('!', 'not |').replace('&(', 'all([').replace('|(', 'any([').replace(')', '])'))
+        
