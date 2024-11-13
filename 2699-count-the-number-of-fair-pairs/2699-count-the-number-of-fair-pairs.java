c844903a0f2d1b56148cb689546d9fb718fa2458
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2024-11-14 01:06:57 +0530

    Time: 24 ms (81.82%), Space: 57.7 MB (12.81%) - LeetHub

---
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    long countLess(int[] nums, int val) {
+        long res = 0;
+        for (int i = 0, j = nums.length - 1; i < j; ++i) {
+            while (i < j && nums[i] + nums[j] > val)
+                --j;
+            res += j - i;
+        }
+        return res;
+    }
+
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        return countLess(nums, upper) - countLess(nums, lower - 1);
+    }
+
+}
